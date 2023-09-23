42a1af1d6100e96a2eccd6d374976f9ccfca3b62
Nikita_kruglov <nikitakruglov0477@gmail.com>
2023-09-23 09:18:34 +0500

    Give a personalised hello.

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 #include &lt;string.h&gt;
 int main(int argc, char **argv)
 {
-char name[255];
-printf(&quot;Enter your name: &quot;);
-fgets(name, 255, stdin);
-printf(&quot;length = %d\n&quot;, strlen(name)); /* debug line */
-name[strlen(name)-1] = &#39;\0&#39;; /* remove the newline at the end */
-printf(&quot;Hello %s!\n&quot;, name);
-return 0;
+printf(&quot;Enter your name: &quot;);char name[255];	char name[255];
+	printf(&quot;Enter your name: &quot;);
+	fgets(name, 255, stdin);
+	printf(&quot;length = %d\n&quot;, strlen(name)); /* debug line */
+	name[strlen(name)-1] = &#39;\0&#39;; /* remove the newline at the end */
+	printf(&quot;Hello %s!\n&quot;, name);
+	return 0;
 }
